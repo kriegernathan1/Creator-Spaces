5c610dce1ceb81027ecec2a6071bd501f06a586f
Nathan Krieger <nkrieger0444@gmail.com>
2023-12-07 20:02:32 -0800

    updated prebuilt UI space name; setup space resolver for prototype

--- space-resolver.js
+++ space-resolver.js
@@ -1,45 +1,14 @@
 const sqlite3 = require("sqlite3");
 const express = require("express");
 const app = express();
+const port = 3001;
 
-app.use(express.static(__dirname));
+const prebuiltSpaceDstPath = "/prebuilt-space/dist";
+app.use(express.static(__dirname + prebuiltSpaceDstPath));
 
-const port = 3000;
-
-const db = new sqlite3.Database("spaces.db", sqlite3.OPEN_READWRITE);
-
-const subdomainToFeMap = new Map();
-(function createMapping() {
-  {
-    db.all("SELECT * FROM spaces", [], (err, rows) => {
-      rows.forEach((row) =>
-        subdomainToFeMap.set(row.subdomain, row.feEntryPoint)
-      );
-    });
-  }
-})();
-
-/**
- *
- * @param {URL} url
- */
-function getSubdomain(url) {
-  return url.host.split(".")[0];
-}
-
+// For prototype we only need to support serving the 'solution-space' that is driven through configuration
 app.get("/", (req, res) => {
-  const fullUrl = new URL(
-    req.protocol + "://" + req.get("host") + req.originalUrl
-  );
-  const subdomain = getSubdomain(fullUrl);
-  const entryPoint = subdomainToFeMap.get(subdomain);
-
-  if (entryPoint) {
-    const path = __dirname + entryPoint;
-    res.sendFile(path);
-  } else {
-    res.send("Unable to resolve subdomain");
-  }
+  res.send(prebuiltSpaceDstPath + "/index.html");
 });
 
 app.listen(port, () => {
