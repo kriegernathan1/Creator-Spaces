c82b0ef0894e11b340ed06f91fb2558b4dca2bc6
Nathan Krieger <nkrieger0444@gmail.com>
2023-12-07 21:44:02 -0800

    prepare www and prebuilt space for dev

--- prebuilt-space/src/views/HomeView.vue
+++ prebuilt-space/src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    Welcome to the pre-built creator space solution
   </main>
 </template>
