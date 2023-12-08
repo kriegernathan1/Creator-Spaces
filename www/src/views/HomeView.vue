1826e4aed076ce2ba479a7276f74f9827bf01ebf
Nathan Krieger <nkrieger0444@gmail.com>
2023-12-07 21:30:36 -0800

    get www ready for dev

--- www/src/views/HomeView.vue
+++ www/src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    Welcome To Creator spaces
+  </div>
 </template>
+
+
+<style lang="scss" scoped></style>
